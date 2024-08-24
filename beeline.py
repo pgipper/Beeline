a90492f3de8c6013c3f024ad2fe4b193a13d52ed
pgipper <peter.gipper@wheregroup.com>
2024-08-24 17:09:39 +0200

    move action to plugin toolbar, reduce noise

--- beeline.py
+++ beeline.py
@@ -19,166 +19,41 @@
  *                                                                         *
  ***************************************************************************/
 """
-from qgis.PyQt.QtCore import *
-from qgis.PyQt.QtGui import *
-from qgis.PyQt.QtWidgets import *
-from qgis.gui import QgsMessageBar
-from qgis.core import *
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QProgressBar
+from qgis.PyQt.QtWidgets import QAction
+from qgis.core import Qgis, QgsProject, QgsVectorLayer, QgsFeature, QgsVectorFileWriter, QgsPointXY, QgsGeometry, \
+    QgsDistanceArea
+from qgis.gui import QgisInterface
+
 # Initialize Qt resources from file resources.py
 from Beeline import resources_rc
 # Import the code for the dialog
 from Beeline.beeline_dialog import BeelineDialog
-# Import libs and the external geographiclib
-import timeit, math, os.path
+import os.path
 
 
 class Beeline:
     """QGIS Plugin Implementation."""
 
-    def __init__(self, iface):
-        """Constructor.
-
-        :param iface: An interface instance that will be passed to this class
-            which provides the hook by which you can manipulate the QGIS
-            application at run time.
-        :type iface: QgsInterface
-        """
-        # Save reference to the QGIS interface
+    def __init__(self, iface: QgisInterface):
         self.iface = iface
-        # initialize plugin directory
-        self.plugin_dir = os.path.dirname(__file__)
-        # initialize locale
-        locale = QSettings().value('locale/userLocale')[0:2]
-        locale_path = os.path.join(
-            self.plugin_dir,
-            'i18n',
-            'Beeline_{}.qm'.format(locale))
 
-        if os.path.exists(locale_path):
-            self.translator = QTranslator()
-            self.translator.load(locale_path)
-
-            if qVersion() > '4.3.3':
-                QCoreApplication.installTranslator(self.translator)
-
-        # Declare instance attributes
-        self.actions = []
-        self.menu = self.tr(u'&Beeline')
-        # Add a toolbar
-        self.toolbar = self.iface.addToolBar(u'Beeline')
-        self.toolbar.setObjectName(u'Beeline')
-
-        # Create the dialog (after translation) and keep reference
+        # Create the dialog and keep reference
         self.dlg = BeelineDialog()
-
-    # noinspection PyMethodMayBeStatic
-    def tr(self, message):
-        """Get the translation for a string using Qt translation API.
-
-        We implement this ourselves since we do not inherit QObject.
-
-        :param message: String for translation.
-        :type message: str, QString
-
-        :returns: Translated version of message.
-        :rtype: QString
-        """
-        # noinspection PyTypeChecker,PyArgumentList,PyCallByClass
-        return QCoreApplication.translate('Beeline', message)
-
-
-    def add_action(
-        self,
-        icon_path,
-        text,
-        callback,
-        enabled_flag=True,
-        add_to_menu=True,
-        add_to_toolbar=True,
-        status_tip=None,
-        whats_this=None,
-        parent=None):
-        """Add a toolbar icon to the toolbar.
-
-        :param icon_path: Path to the icon for this action. Can be a resource
-            path (e.g. ':/plugins/foo/bar.png') or a normal file system path.
-        :type icon_path: str
-
-        :param text: Text that should be shown in menu items for this action.
-        :type text: str
-
-        :param callback: Function to be called when the action is triggered.
-        :type callback: function
-
-        :param enabled_flag: A flag indicating if the action should be enabled
-            by default. Defaults to True.
-        :type enabled_flag: bool
-
-        :param add_to_menu: Flag indicating whether the action should also
-            be added to the menu. Defaults to True.
-        :type add_to_menu: bool
-
-        :param add_to_toolbar: Flag indicating whether the action should also
-            be added to the toolbar. Defaults to True.
-        :type add_to_toolbar: bool
-
-        :param status_tip: Optional text to show in a popup when mouse pointer
-            hovers over the action.
-        :type status_tip: str
-
-        :param parent: Parent widget for the new action. Defaults None.
-        :type parent: QWidget
-
-        :param whats_this: Optional text to show in the status bar when the
-            mouse pointer hovers over the action.
-
-        :returns: The action that was created. Note that the action is also
-            added to self.actions list.
-        :rtype: QAction
-        """
-
-        icon = QIcon(icon_path)
-        action = QAction(icon, text, parent)
-        action.triggered.connect(callback)
-        action.setEnabled(enabled_flag)
-
-        if status_tip is not None:
-            action.setStatusTip(status_tip)
-
-        if whats_this is not None:
-            action.setWhatsThis(whats_this)
-
-        if add_to_toolbar:
-            self.toolbar.addAction(action)
-
-        if add_to_menu:
-            self.iface.addPluginToVectorMenu(
-                self.menu,
-                action)
-
-        self.actions.append(action)
-
-        return action
 
     def initGui(self):
         """Create the menu entries and toolbar icons inside the QGIS GUI."""
         icon_path = ':/plugins/Beeline/icon.svg'
-        self.add_action(
-            icon_path,
-            text=self.tr(u'Connect points along great circles'),
-            callback=self.run,
-            parent=self.iface.mainWindow())
-
+        self.beelineAction = QAction(QIcon(icon_path), 'Beeline', self.iface.mainWindow())
+        self.beelineAction.triggered.connect(self.run)
+        self.iface.addPluginToMenu('Beeline', self.beelineAction)
+        self.iface.addToolBarIcon(self.beelineAction)
 
     def unload(self):
         """Removes the plugin menu item and icon from QGIS GUI."""
-        for action in self.actions:
-            self.iface.removePluginVectorMenu(
-                self.tr(u'&Beeline'),
-                action)
-            self.iface.removeToolBarIcon(action)
-        # remove the toolbar
-        del self.toolbar
+        self.iface.removePluginMenu('Beeline', self.beelineAction)
+        self.iface.removeToolBarIcon(self.beelineAction)
 
     def showMessage(self, message, level=Qgis.Info):
         """Pushes a message to the Message Bar"""
@@ -206,8 +81,6 @@ class Beeline:
         # See if OK was pressed
         if result:
 
-            tic=timeit.default_timer()
-
             # Check for a valid Input Layers in the project
             point_layers = []
             layers = QgsProject.instance().mapLayers()
@@ -217,7 +90,7 @@ class Beeline:
                 if type(layers[key])== QgsVectorLayer and layers[key].geometryType() == 0:
                     point_layers.append(layers[key])
             if len(point_layers) == 0:
-                self.showMessage(self.tr('No layers to process. Please add a point layer to your project.'), Qgis.Warning)
+                self.showMessage('No layers to process. Please add a point layer to your project.', Qgis.Warning)
                 return
 
             # Get input layer by name (index may change)
@@ -226,12 +99,12 @@ class Beeline:
 
             # Check if CRS is WGS84 (EPSG:4326)
             if inputLayer.crs().authid() != u'EPSG:4326':
-                self.showMessage(self.tr('Input point layer must be in geographic coordinates (WGS 84, EPSG 4326).'), Qgis.Warning)
+                self.showMessage('Input point layer must be in geographic coordinates (WGS 84, EPSG 4326).', Qgis.Warning)
                 return
 
             # Check if output location is set
             elif (self.dlg.ui.shapefileOutput.isChecked() and self.dlg.ui.outputFilename.text() == ''):
-                self.showMessage(self.tr('Error, no valid shapefile name for output'), Qgis.Warning)
+                self.showMessage('Error, no valid shapefile name for output', Qgis.Warning)
                 return
 
             # Get output filename
@@ -284,8 +157,6 @@ class Beeline:
                     pr.addFeatures([outFeat])
                 k += 1
             self.iface.messageBar().clearWidgets()
-            toc=timeit.default_timer()
-            print("processing time: ", toc-tic)
 
             # Handle output
             if self.dlg.ui.memoryLayerOutput.isChecked():  # Load memory layer in canvas
@@ -300,7 +171,7 @@ class Beeline:
                     QgsProject.instance().addMapLayer(savedLayer)
 
             # Show success message
-            self.showMessage(self.tr('Completed.'), Qgis.Success)
+            self.showMessage('Completed.', Qgis.Success)
             self.dlg.close()
 
     def createGeodesicLine(self, point1: QgsPointXY, point2: QgsPointXY, segmentSize: float = 100000.0) -> QgsGeometry:
